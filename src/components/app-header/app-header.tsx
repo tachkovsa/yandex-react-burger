4ef28466314c6b01ac6c3acd267301291735d6aa
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-01-22 16:39:02 +0300

    feat: Add AppHeader component

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,45 @@
+import { BurgerIcon, ListIcon, Logo, ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+
+import headerStyles from './app-header.module.css';
+
+const AppHeader = ({ activeTab, selectTab }) => {
+
+    return (
+        <header className={`${headerStyles.header} pt-4 pb-4`}>
+            <div className={`${headerStyles.content} `}>
+                <div className={`${headerStyles.side} ${headerStyles.leftSide}`}>
+                    <button
+                        className={`${headerStyles.menuBtn} ${activeTab === 'constructor' ? headerStyles.menuBtnActive : ''} pt-4 pr-5 pb-4 pl-5`}
+                        type="button"
+                        onClick={() => selectTab('constructor')}
+                    >
+                        <BurgerIcon type={activeTab === 'constructor' ? 'primary' : 'secondary'} />
+                        <span className={`${headerStyles.menuBtnTitle} ml-2`}>Конструктор</span>
+                    </button>
+                    <button
+                        className={`${headerStyles.menuBtn} ${activeTab === 'order_list' ? headerStyles.menuBtnActive : ''} pt-4 pr-5 pb-4 pl-5`}
+                        type="button"
+                        onClick={() => selectTab('order_list')}
+                    >
+                        <ListIcon type={activeTab === 'order_list' ? 'primary' : 'secondary'}/>
+                        <span className={`${headerStyles.menuBtnTitle} ml-2`}>Лист заказов</span>
+                    </button>
+                </div>
+                <div className={`${headerStyles.side} ${headerStyles.rightSide}`}>
+                    <button  className={`${headerStyles.menuBtn} ${activeTab === 'profile' ? headerStyles.menuBtnActive : ''} pt-4 pr-5 pb-4 pl-5`}
+                        type="button"
+                        onClick={() => selectTab('profile')}
+                    >
+                        <ProfileIcon type={activeTab === 'profile' ? 'primary' : 'secondary'}/>
+                        <span className={`${headerStyles.menuBtnTitle} ml-2`}>Личный кабинет</span>
+                    </button>
+                </div>
+                <div className={`${headerStyles.side} ${headerStyles.middleSide}`}>
+                    <Logo />
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default AppHeader;

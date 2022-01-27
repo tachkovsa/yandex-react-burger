fc7277ef52dfb008a8d3d04dacf01ea3eccb14bc
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-01-27 19:03:31 +0300

    ref: Move scrollbar to inner container

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -10,6 +10,8 @@ import burgerConstructorStyles from './burger-constructor.module.css';
 
 const BurgerConstructor = ({ ingredients, basket }) => {
     const [totalPrice, setTotalPrice] = useState(0);
+    const [topBun, setTopBun] = useState<any>(undefined);
+    const [bottomBun, setBottomBun] = useState<any>(undefined);
 
     useEffect(() => {
         setTotalPrice(
@@ -17,37 +19,70 @@ const BurgerConstructor = ({ ingredients, basket }) => {
                 .map(el => ingredients.find(i => i._id === el._id).price)
                 .reduce((acc, price) => acc + price)
         );
+
+        let bun;
+        const buns = ingredients.filter(i => i.type === 'bun');
+        buns.some(element => {
+            if (basket.find(b => b._id === element._id)) {
+                bun = element;
+                return true;
+            }
+        });
+        setTopBun(bun);
+        setBottomBun(bun);
+
     }, [ingredients, basket]);
 
     return (
         <>
-            <SimpleBar className={classNames(burgerConstructorStyles.basketListContainer)} >
-                <div className={classNames(burgerConstructorStyles.basketList, 'mr-4')}>
-                    {basket.map((elem, index) => {
-                        const ingredient = ingredients.find(i => i._id === elem._id);
-
-                        const isFirst = index === 0;
-                        const isLast = index === (basket.length - 1);
-                        
-                        return (
-                            <div className={classNames(burgerConstructorStyles.basketListElem, 'ml-4')} 
-                                key={`${index}_${elem._id}`}>
-                                {(!isLast && !isFirst) && <DragIcon type="primary" />}
-                                <div className={classNames(burgerConstructorStyles.bullet, (isFirst || isLast ? 'ml-8' : 'ml-2'))}>
-                                    <ConstructorElement
-                                        type={isFirst ? 'top' : (isLast ? 'bottom' : undefined)}
-                                        isLocked={isFirst || isLast}
-                                        text={`${ingredient.name}${isLast && ingredient.type === 'bun' ? ' (низ)' : ingredient.type === 'bun' ? ' (верх)' : ''}`}
-                                        price={ingredient.price}
-                                        thumbnail={ingredient.image}
-                                    />
+            <div className={classNames(burgerConstructorStyles.basketListContainer)}>
+                {topBun && (
+                    <div className={classNames(burgerConstructorStyles.bulletEdge, 'mr-4', 'mb-4')}>
+                        <ConstructorElement
+                            type={'top'}
+                            isLocked={true}
+                            text={`${topBun.name} (верх)`}
+                            price={topBun.price}
+                            thumbnail={topBun.image}
+                        />
+                    </div>
+                )}
+                <SimpleBar className={classNames(burgerConstructorStyles.basketListBar)}>
+                    <div className={classNames(burgerConstructorStyles.basketList, 'mr-4')}>
+                        {basket.slice(1, -1).map((elem, index) => {
+                            const ingredient = ingredients.find(i => i._id === elem._id);
+                            
+                            return (
+                                <div className={classNames(burgerConstructorStyles.basketListElem, 'ml-4')} 
+                                    key={`${index}_${elem._id}`}>
+                                    <DragIcon type="primary" />
+                                    <div className={classNames(burgerConstructorStyles.bullet, 'ml-2')}>
+                                        <ConstructorElement
+                                            isLocked={false}
+                                            text={`${ingredient.name}`}
+                                            price={ingredient.price}
+                                            thumbnail={ingredient.image}
+                                        />
+                                    </div>
                                 </div>
-                            </div>
-                        );
+                            );
                         }
-                    )}
-                </div>
-            </SimpleBar>
+                        )}
+                    </div>
+                </SimpleBar>
+                {bottomBun && (
+                    <div className={classNames(burgerConstructorStyles.bulletEdge, 'mr-4', 'mt-4')}>
+                        <ConstructorElement
+                            type={'bottom'}
+                            isLocked={true}
+                            text={`${bottomBun.name} (низ)`}
+                            price={bottomBun.price}
+                            thumbnail={bottomBun.image}
+                        />
+                    </div>
+                )}
+            </div>
+
             <div className={classNames(burgerConstructorStyles.orderInfo, 'mt-10', 'mr-4')}>
                 <div className={classNames(burgerConstructorStyles.orderInfoPrice, 'mr-10')}>
                     <span className={classNames('text', 'text_type_digits-medium', 'mr-2')}>{totalPrice}</span>

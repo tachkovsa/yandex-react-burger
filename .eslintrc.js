85d855b7e607c181ac095094c23540312b0f793d
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-02-18 02:23:51 +0300

    chore(eslint): Turn off 'jsx-a11y/no-static-element-interactions' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
     'no-underscore-dangle': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
   },
 };

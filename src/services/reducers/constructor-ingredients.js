6c8c237b64cc2c29566daf28d3f06877b6158e30
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-02-18 16:09:59 +0300

    ref(constructorIngredientsReducer): Rework actions processing

--- src/services/reducers/constructor-ingredients.js
+++ src/services/reducers/constructor-ingredients.js
@@ -1,36 +1,46 @@
-import { v4 as uuidv4 } from 'uuid';
 import Actions from '../actions';
 
 const initialState = {
-  ingredients: [],
+  basket: [],
 };
 
 export const constructorIngredientsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case Actions.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: [
-          ...state.ingredients,
-          {
-            _uid: uuidv4(),
-            _id: action.payload._id,
-            type: action.payload.type,
-          }],
-      };
-    case Actions.REMOVE_INGREDIENT: {
-      const ingredientIndex = state.ingredients
-        .findIndex((ingredient) => ingredient._uid === action.payload);
+    case Actions.SET_BUN: {
+      const bunIndex = state.basket.findIndex((b) => b.type === 'bun');
+      const bun = action.payload;
+
+      const updatedBasket = [...state.basket];
+      if (bunIndex >= 0) {
+        updatedBasket.splice(bunIndex, 1, bun);
+      } else {
+        updatedBasket.push(bun);
+      }
 
       return {
         ...state,
-        ingredients: [...state.ingredients].splice(ingredientIndex, 1),
+        basket: updatedBasket,
+      };
+    }
+    case Actions.ADD_INGREDIENT:
+      return {
+        ...state,
+        basket: [
+          ...state.basket,
+          action.payload,
+        ],
+      };
+    case Actions.REMOVE_INGREDIENT: {
+      return {
+        ...state,
+        basket: state.basket.filter((ingredient) => ingredient._uid !== action.payload),
       };
     }
     case Actions.RESET_INGREDIENTS:
       return {
         ...state,
-        ingredients: [],
+        basket: [],
+        readyForOrder: false,
       };
     default:
       return state;

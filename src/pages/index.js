640f99c23700406975e148724d538bb47eac6fd3
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-04 20:01:19 +0300

    feat(pages): Add index file for pages

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import { MainPage } from './main/main';
+import { LoginPage } from './login';
+import { RegisterPage } from './register';
+import { ForgotPasswordPage } from './forgot-password';
+import { ResetPasswordPage } from './reset-password';
+
+export {
+  MainPage, LoginPage, RegisterPage, ForgotPasswordPage, ResetPasswordPage,
+};

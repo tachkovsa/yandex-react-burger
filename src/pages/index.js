59f3fa1f98786ea445e9c50120b01c021406de21
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-04 22:44:31 +0300

    feat(pages): Add ingredients page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import { LoginPage } from './login';
 import { RegisterPage } from './register';
 import { ForgotPasswordPage } from './forgot-password';
 import { ResetPasswordPage } from './reset-password';
+import { IngredientsPage } from './ingredients';
 
 export {
-  MainPage, LoginPage, RegisterPage, ForgotPasswordPage, ResetPasswordPage,
+  MainPage, LoginPage, RegisterPage, ForgotPasswordPage, ResetPasswordPage, IngredientsPage,
 };

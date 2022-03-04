d78b7b28f360dfe2f459a624d2f61cee7db77e42
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-04 21:21:42 +0300

    feat(routes): Add main, login, register, forgot-password and reset-password routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import { MainPage } from './pages/main';
+import { Route, Switch } from 'react-router-dom';
+import {
+  MainPage, LoginPage, RegisterPage, ForgotPasswordPage, ResetPasswordPage,
+} from './pages';
 
 export function Routes() {
   return (
     <Switch>
       {/* Главная страница, конструктор бургеров */}
       <Route exact path="/" component={MainPage} />
-      {/* TODO: Страница авторизации */}
-      {/* <Route exact path="/login" component /> */}
-      {/* TODO: Страница регистрации */}
-      {/* <Route exact path="/register" component /> */}
-      {/* TODO: Страница восстановления пароля */}
-      {/* <Route exact path="/forgot-password" component /> */}
-      {/* TODO: Страница сброса пароля */}
-      {/* <Route exact path="/reset-password" component /> */}
+      {/* Страница авторизации */}
+      <Route exact path="/login" component={LoginPage} />
+      {/* Страница регистрации */}
+      <Route exact path="/register" component={RegisterPage} />
+      {/* Страница восстановления пароля */}
+      <Route exact path="/forgot-password" component={ForgotPasswordPage} />
+      {/* Страница сброса пароля */}
+      <Route exact path="/reset-password" component={ResetPasswordPage} />
       {/* TODO: Страница с настройками профиля пользователя */}
       {/* <Route exact path="/profile" component /> */}
       {/* TODO: Страница ингредиента */}

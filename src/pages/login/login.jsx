d06a2c7a1ebdbc61d4f39b73565d0b97942fa7c9
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-07 13:04:40 +0300

    feat(auth): Add additional functionality to login page

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,29 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { EmailInput, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import {
+  PasswordInput, Button, Input,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { validateEmail } from '../../utils/validation';
 
 import commonStyles from '../common.module.css';
 import styles from './login.module.css';
+import { loginUser } from '../../services/actions/auth';
 
 export function LoginPage() {
-  const onChangeEmail = (e) => {};
-  const onChangePassword = (e) => {};
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { loading, error, user } = useSelector((state) => state.auth);
+
+  const isEmailValid = useCallback(() => !!validateEmail(email), [email]);
+  const isFormValid = useCallback(() => isEmailValid() && password !== '', [isEmailValid, password]);
+
+  const login = (e) => {
+    e.preventDefault();
+    dispatch(loginUser({ email, password }));
+  };
+
+  useEffect(() => {
+    if (user) {
+      history.replace('/');
+    }
+  }, [user, history]);
 
   return (
     <div className={commonStyles.content}>
       <div className={styles.loginContainer}>
-        <form className={classNames('form-fields_width_100', 'mb-20')}>
+        <form className={classNames('form-fields_width_100', 'mb-20')} onSubmit={(e) => login(e)}>
           <div className={classNames('text', 'text_type_main-medium', 'mb-6', 'text_align_center')}>Вход</div>
           <div className={classNames('mb-6')}>
-            <EmailInput onChange={onChangeEmail} value="" name="email" />
+            <Input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Укажите e-mail"
+              error={email !== '' && !isEmailValid()}
+              errorText="Введён некорректный email"
+              disabled={loading}
+              success={isEmailValid()}
+            />
           </div>
           <div className={classNames('mb-6')}>
-            <PasswordInput onChange={onChangePassword} value="" name="password" className="mb-6" />
+            <PasswordInput
+              onChange={(e) => setPassword(e.target.value)}
+              disabled={loading}
+              value={password}
+              name="password"
+              className="mb-6"
+            />
           </div>
           <div className="text_align_center">
-            <Button type="primary" size="large">
+            {error && <p className="text text_type_main-default text_color_inactive text_color_error mb-6">{error}</p>}
+            <Button type="primary" size="large" disabled={!isFormValid() || loading}>
               Войти
             </Button>
           </div>

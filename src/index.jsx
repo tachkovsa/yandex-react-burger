33bba6d0b85d1f22201734afc123f48d1e916b12
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-09 21:44:14 +0300

    ref(store): Change store imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
 
 import App from './components/app/app';
-import rootReducer from './services/reducers';
+import { store } from './store';
 
 import './index.css';
-
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(
-    applyMiddleware(thunk),
-  ),
-);
 
 ReactDOM.render(
   <Provider store={store}>

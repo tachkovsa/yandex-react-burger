b08f87d863030652b6e9649f0560a4e729e53249
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-12 12:09:29 +0300

    feat(auth): Add loader to scenarios, which need authorization state

--- src/components/protected-route.jsx
+++ src/components/protected-route.jsx
@@ -1,18 +1,35 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import classNames from 'classnames';
 import { auth } from '../services/auth';
 import { fetchUser } from '../store/actions/auth';
+import commonStyles from '../pages/common.module.css';
 
 export function ProtectedRoute({ children, accessType = 'anonymous', ...rest }) {
   const dispatch = useDispatch();
 
-  const { user } = useSelector((state) => state.auth);
+  const { user, loading } = useSelector((state) => state.auth);
   const { accessToken, refreshToken } = auth();
 
+  const [loadingText, setLoadingText] = useState('Проверяем авторизацию...');
   const isAuth = useCallback(() => !!((accessToken || refreshToken) && user), [accessToken, refreshToken, user]);
+
+  // TODO: Move this code to helper
+  useEffect(() => {
+    let interval; let
+      tick = 0;
+    if (loading) {
+      interval = setInterval(() => {
+        setLoadingText(`Проверяем авторизацию${'.'.repeat(tick)}`);
+        tick = tick === 3 ? 0 : ++tick;
+      }, 450);
+    }
+
+    return () => clearInterval(interval);
+  }, [setLoadingText, loading]);
 
   useEffect(() => {
     if ((accessToken || refreshToken) && !user) {
@@ -21,8 +38,13 @@ export function ProtectedRoute({ children, accessType = 'anonymous', ...rest }) 
   }, [accessToken, dispatch, refreshToken, user]);
 
   const render = () => {
-    let elementToRender = (<Route {...rest} render={() => children} />);
+    if (accessType !== 'anonymous' && loading) {
+      return (
+        <div className={classNames(commonStyles.loader, 'text', 'text_type_main-default')}>{loadingText}</div>
+      );
+    }
 
+    let elementToRender = (<Route {...rest} render={() => children} />);
     switch (accessType) {
       case 'authorized':
         if (!isAuth()) {

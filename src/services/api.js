c85bc7eb8d2d50635b9319255b199735fa7ce5c7
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-12 14:04:57 +0300

    feat(api): Add checkResponse function

--- src/services/api.js
+++ src/services/api.js
@@ -4,27 +4,34 @@ import { auth, setTokens } from './auth';
 import { store } from '../store';
 import { resetAuth } from '../store/actions/auth';
 
+const checkResponse = (res) => {
+  if (res && res.ok) {
+    return res.json();
+  }
+
+  if (res.status === 401) {
+    store.dispatch(resetAuth());
+  }
+
+  return Promise.reject(new Error(res.statusText));
+};
+
 const restoreSession = async ({ refreshToken }) => {
   const success = await fetch(`${domainURL}/api/auth/token`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ token: refreshToken }),
-  }).then((res) => {
-    if (res && res.ok) {
-      return res.json();
-    }
+  }).then(checkResponse)
+    .then((parsedResponse) => {
+      if (parsedResponse.success) {
+        const { accessToken, refreshToken } = parsedResponse;
+        setTokens({ accessToken, refreshToken });
 
-    return Promise.reject(new Error(res.statusText));
-  }).then((parsedResponse) => {
-    if (parsedResponse.success) {
-      const { accessToken, refreshToken } = parsedResponse;
-      setTokens({ accessToken, refreshToken });
+        return true;
+      }
 
-      return true;
-    }
-
-    return Promise.reject(parsedResponse.message);
-  }).catch((err) => false);
+      return Promise.reject(parsedResponse.message);
+    }).catch((err) => false);
 
   return success;
 };
@@ -46,15 +53,7 @@ export const request = async ({
       ...headers,
     },
     body: ['GET', 'HEAD'].includes(method) && !!body ? null : body,
-  }).then((res) => {
-    if (res) {
-      if (res.status === 401) {
-        store.dispatch(resetAuth());
-      }
-      return res.json();
-    }
-    return Promise.reject(new Error(res.statusText));
-  });
+  }).then(checkResponse);
 
   return result;
 };

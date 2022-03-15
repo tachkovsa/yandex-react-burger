f0b497ddb6a3a18bcb2a40ffa93d879b24ff597a
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-15 18:25:33 +0300

    ref(utils): Add typings to validateEmail function

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,4 +1,4 @@
-export const validateEmail = (email) => String(email)
+export const validateEmail = (email: string) => String(email)
   .toLowerCase()
   .match(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,

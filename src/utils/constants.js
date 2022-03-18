bf49a5df1e3fada7bebf4fb6ceb833b7066a9dd8
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-18 08:38:45 +0300

    ref(utils): Add typings to constants

--- src/utils/constants.js
+++
@@ -1,2 +0,0 @@
-export const domainURL = 'https://norma.nomoreparties.space';
-export const AUTH_TOKEN_ALIVE_TIME = (20 * 60) * 1000;
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const domainURL: string = 'https://norma.nomoreparties.space';
+export const AUTH_TOKEN_ALIVE_TIME: number = (20 * 60) * 1000;

307ff80989dab6de6c6c52344b7c67878f1face7
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-03-22 09:22:56 +0300

    feat(utils): Add objectHasKeys function

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,3 +3,22 @@ export const validateEmail = (email: string) => String(email)
   .match(
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   );
+
+/**
+ * Функция проверяет наличие переданных ключей `keys` в объекте `object`
+ * @param object Объект, в котором идёт поиск ключей
+ * @param keys Массив ключей, которые ищутся в объекте
+ */
+export const objectHasKeys = (object: Object | null, keys: string[]) => {
+  if (object === null) {
+    return false;
+  }
+
+  for (const key of keys) {
+    if (!(key in object)) {
+      return false;
+    }
+  }
+
+  return true;
+};

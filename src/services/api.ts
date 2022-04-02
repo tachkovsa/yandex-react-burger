ade93857026d5c714b1dd66ed99743f9f4977f13
Тачков Сергей Александрович <tachkovsa@ya.ru>
2022-04-02 13:31:05 +0300

    feat(api): Add checkResponseType function, because I can. :-)

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,16 +3,29 @@ import { auth, setTokens } from './auth';
 
 import { store } from '../store';
 import { resetAuth } from '../store/actions/auth';
-import { ITokenResponse } from '../utils/interfaces/api.interface';
+import { ICommonResponse, ITokenResponse } from '../utils/interfaces/api.interface';
 import { objectHasKeys } from '../utils/validation';
+
+const checkResponseType = (unknownRes: unknown): ICommonResponse | Promise<never> => {
+  if (typeof unknownRes === 'object' && unknownRes !== null
+      && objectHasKeys(unknownRes, ['success'])) {
+    const knownResponse = unknownRes as ICommonResponse;
+
+    if (knownResponse.success) {
+      return knownResponse;
+    }
+    return Promise.reject(knownResponse.message);
+  }
+  return Promise.reject(new Error('Unknown response type'));
+};
 
 const checkResponse = (res: Response) => {
   if (res && res.ok) {
-    return res.json();
+    return res.json().then(checkResponseType);
   }
 
   if (res.status === 401) {
-    store.dispatch(resetAuth());
+    store.dispatch(resetAuth(res.statusText));
   }
 
   return Promise.reject(new Error(res.statusText));
@@ -24,20 +37,14 @@ const restoreSession = async ({ refreshToken }: { refreshToken: string }) => {
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ token: refreshToken }),
   }).then(checkResponse)
-    .then((parsedResponse: unknown) => {
-      if (typeof parsedResponse === 'object'
-          && objectHasKeys(parsedResponse, ['accessToken', 'refreshToken'])
-      ) {
+    .then((parsedResponse) => {
+      if (objectHasKeys(parsedResponse, ['accessToken', 'refreshToken'])) {
         const tokenResponse = parsedResponse as ITokenResponse;
 
-        if (tokenResponse.success) {
-          const { accessToken, refreshToken } = tokenResponse;
-          setTokens({ accessToken, refreshToken });
+        const { accessToken, refreshToken } = tokenResponse;
+        setTokens({ accessToken, refreshToken });
 
-          return true;
-        }
-
-        return Promise.reject(tokenResponse.message);
+        return true;
       }
 
       return Promise.reject(new Error('Unknown response'));
@@ -48,7 +55,7 @@ const restoreSession = async ({ refreshToken }: { refreshToken: string }) => {
 
 export const request = async ({
   url = '', method = 'GET', body = '', headers = {},
-}): Promise<unknown> => {
+}) => {
   const { accessToken, refreshToken } = auth();
 
   if (!accessToken && refreshToken) {
